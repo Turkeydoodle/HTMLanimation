c6b718942ffccf5f67b1fc10da78b0ccfb251da6
Kevin Todd-Yu <kevintoddyu@gmail.com>
2026-01-24 09:37:13 -0800

    Added a mouse tracker

--- code.js
+++ code.js
@@ -1,1 +1,8 @@
 console.log("Javascript successfully connected.");
+function mousetracker(event) {
+    let x = event.pageX;
+    let y = event.pageY;
+    let display = document.getElementById("coordinates");
+    display.innerHTML = "X: " + x + " Y: " + y;
+}
+document.addEventListener("mousemove", mousetracker);
