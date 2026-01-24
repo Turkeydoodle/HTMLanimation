14ee0c4dfb47b4ec31abd43d1d974a46399da26f
Kevin Todd-Yu <kevintoddyu@gmail.com>
2026-01-24 10:04:33 -0800

    when mouse is down, a square follows the cursor

--- code.js
+++ code.js
@@ -7,12 +7,25 @@ function mousetracker(event) {
     let display = document.getElementById("coordinates");
     display.innerHTML = "X: " + x + " Y: " + y;
 }
+drawing = false;
 function canvasmousetracker(event) {
     let rect = canvas.getBoundingClientRect();
     let x = event.offsetX;
     let y = event.offsetY;
     let display2 = document.getElementById("offsetCoordinates");
     display2.innerHTML = "Offset X: " + x + " Offset Y: " + y;
+    if (drawing) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(x, y, 50, 50);
+    }
+}
+function startdraw(event) {
+    drawing = true;
+}
+function stopdraw(event) {
+    drawing = false;
 }
 canvas.addEventListener("mousemove", canvasmousetracker);
 document.addEventListener("mousemove", mousetracker);
+canvas.addEventListener("mousedown", startdraw);
+canvas.addEventListener("mouseup", stopdraw);
